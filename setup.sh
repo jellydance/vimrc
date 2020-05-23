eac92c50a7bae65530383ffb8456fd9e582aa5e1
jellydance <61858300+jellydance@users.noreply.github.com>
2020-05-23 12:33:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,10 @@ vim +PlugInstall +UpdateRemotePlugins +qa
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p ~/.config/nvim
 cp $DIR/init.vim ~/.config/nvim/init.vim
+
+PYTHON2_PATH="$( whereis python2|cut -d' ' -f2 )"
+PYTHON3_PATH="$( whereis python3|cut -d' ' -f2 )"
+echo "let g:python_host_prog = '$PYTHON2_PATH'" >> ~/.config/nvim/init.vim
+echo "let g:python3_host_prog = '$PYTHON3_PATH'" >> ~/.config/nvim/init.vim
+
 nvim +PlugInstall +UpdateRemotePlugins +qa
